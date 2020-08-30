1d42b25db4e85fc3c025790d330d227de04e56a1
shevkate <herbathday@gmail.com>
2020-08-31 02:11:38 +0500

    server

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const config = require("config");
+const mongoose = require('mongoose');
+const app = express();
+
+const PORT = config.get('port')|| 3000;
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongo'), {
+            useNewUrlParser: true,
+            useUnifiedTopology:true,
+            useCreateIndex: true
+        });
+
+        app.listen(PORT, () => console.log('This app has been started on port ${PORT}'));
+
+    }catch(e){
+        console.log('Error on server', e.message);
+        process.exit(1)
+
+    }
+}
+
+start();
